e818ba25c0bfa544892e5fa789fbcbbd8486ecad
Juan Useche <juanuseche47@gmail.com>
2026-03-07 00:43:38 -0300

    Updated variable

--- src/app/api/auth/signup/actions.ts
+++ src/app/api/auth/signup/actions.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { redirect } from "next/navigation";
 
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = process.env.API_BASE_URL ||'http://localhost:3000';
 
 export async function signup(prevState: any, formData: FormData) {
 
